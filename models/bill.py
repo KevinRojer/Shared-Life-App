05b90a83194a154c4013c4b6da9b03c5543ea447
Kevin Rojer <kevin.rojer@live.com>
2024-03-13 20:45:05 +0100

    Created a bill module.

---
+++ models/bill.py
@@ -0,0 +1,8 @@
+class Bill:
+    """
+    Represents the bill for a given period.
+    """
+    
+    def __init__(self, total_amount, period):
+        self.amount = total_amount
+        self.period = period
