0521059af782b2652c995333f5e622923c877098
Kevin Rojer <kevin.rojer@live.com>
2024-03-13 21:07:20 +0100

    Added total days as parameter to the calculation of payment method in flatmates.

--- models/flatmate.py
+++ models/flatmate.py
@@ -8,9 +8,9 @@ class Flatmate:
         self.days_in_house = days_in_house
 
     
-    def pay_share(self, bill):
+    def pay_share(self, bill, total_days):
         """
         Calculates the share of the bill the flatmate has to pay based on
         the number of days they stayed in the house.
         """
-        return bill.amount * (self.days_in_house / 30)
+        return bill.amount * (self.days_in_house / total_days)
