87471f6b48d938a26be95e1d1fa55c21d536d713
Kevin Rojer <kevin.rojer@live.com>
2024-03-13 20:55:44 +0100

    Implemented the pay_share method for flatmate that calculates the amount to be paid proportionate to the number of days statyed in the house.

--- models/flatmate.py
+++ models/flatmate.py
@@ -2,7 +2,7 @@ class Flatmate:
     """
     Represents a flatmate.
     """
-    
+
     def __init__(self, name, days_in_house):
         self.name = name
         self.days_in_house = days_in_house
@@ -13,4 +13,4 @@ class Flatmate:
         Calculates the share of the bill the flatmate has to pay based on
         the number of days they stayed in the house.
         """
-        pass
+        return bill.amount * (self.days_in_house / 30)
