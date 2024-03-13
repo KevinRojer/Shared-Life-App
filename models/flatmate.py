25bc9ba55e5a6dfa09a8a597f5c7113a2a6bb1cb
Kevin Rojer <kevin.rojer@live.com>
2024-03-13 20:45:31 +0100

    Created a flatmate module.

---
+++ models/flatmate.py
@@ -0,0 +1,16 @@
+class Flatmate:
+    """
+    Represents a flatmate.
+    """
+    
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    
+    def pay_share(self, bill):
+        """
+        Calculates the share of the bill the flatmate has to pay based on
+        the number of days they stayed in the house.
+        """
+        pass
