b3e7726412109d9c37667507645fe1100ea5c92c
Kevin Rojer <kevin.rojer@live.com>
2024-03-13 20:52:14 +0100

    Created a reporting module

---
+++ models/report.py
@@ -0,0 +1,15 @@
+class ReportGenerator:
+    """
+    Handles the creation of PDF reports.
+    """
+
+    def __init__(self, report_name):
+        self.report_name = report_name
+
+
+    def generate_pdf(self, flatmates, bill):
+        """
+        Generates a PDF report stating the names of the flatmates, 
+        the period, and how much they have to pay.
+        """
+        pass
